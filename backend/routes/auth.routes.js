a91d7fb38307ace18ec693d90fb4afd134955f45
nikhilgupta0836 <ng652403@gmail.com>
2025-10-29 09:09:52 +0530

    added routes for auuthentication

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,7 @@
 import express from "express"
 import { login, logOut, signUp } from "../controllers/auth.controllers.js"
 
-const authRouter=express.Router()
+const authRouter=express.Router() // express.Router() creates a new router object to handle routes related to authentication.
 
 authRouter.post("/signup",signUp)
 authRouter.post("/login",login)
